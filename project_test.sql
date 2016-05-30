ea4c8d202e5142d19007669192b84b426b44f8a7
kshvakov <shvakov@gmail.com>
2016-05-30 15:05:06 +0300

    fix error

--- project_test.sql
+++ project_test.sql
@@ -16,7 +16,7 @@ create or replace function example.test_sum() returns void as $$
 $$ language plpgsql;
 
 
-create or replace function example.test_sum_with_errors() returns void as $$
+create or replace function example.test_sum_fix_errors() returns void as $$
     declare
         a        int;
         b        int;
@@ -26,7 +26,7 @@ create or replace function example.test_sum_with_errors() returns void as $$
         FOR a, b, expected IN VALUES
             (0, 0, 0),
             (1, 1, 2),
-            (2, 2, 5)
+            (2, 2, 4)
         LOOP
             PERFORM assert.equal(expected, example.sum(a, b), 'do you really think so ?');
         END LOOP;
